0ac3b124bca40eccda505857711181393c6642b0
Tony Allevato <allevato@vt.edu>
2013-01-26 10:11:43 -0500

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Queso::Application.config.secret_token = '12e1eae9b78368cef22fe2e4996bdac9db186f5c93eb88f3bbb7b8ce4fffcb8bed56e694edfd66231807c0ed14d600ceb73ec00b602e062b3b75537eab82774e'
